18c4c70cc2e4fa2550c788e0346447f9a1bc89a6
ZWJCSU <43605711+ZWJCSU@users.noreply.github.com>
2020-10-15 07:51:57 +0800

    Update training_loop.py

--- DiffAugment-stylegan2/training/training_loop.py
+++ DiffAugment-stylegan2/training/training_loop.py
@@ -312,7 +312,8 @@ def training_loop(
                 autosummary('Resources/peak_gpu_mem_gb', peak_gpu_mem_op.eval() / 2**30)))
             autosummary('Timing/total_hours', total_time / (60.0 * 60.0))
             autosummary('Timing/total_days', total_time / (24.0 * 60.0 * 60.0))
-
+            misc.save_pkl((G, D, Gs), dnnlib.make_run_dir_path('network-final.pkl%-5d'%cur_tick))
+            
             # Save snapshots.
             if image_snapshot_ticks is not None and (cur_tick % image_snapshot_ticks == 0 or done):
                 grid_fakes = Gs.run(grid_latents, grid_labels, is_validation=True, minibatch_size=sched.minibatch_gpu)
